72a809e2645f8d16e079778cdbbf3fc53e404fdf
Plan49 <anthonyplanisek1@gmail.com>
2023-04-15 15:53:06 -0400

    finished filters

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html'
 })
 export class FiltersComponent {
+  @Output() showCategory = new EventEmitter<string>();
   categories = ['shoes', 'sports'];
 
   constructor() {}
 
   ngOnInit() {}
+
+  onShowCategory(category: string): void {
+    this.showCategory.emit(category);
+  }
 }
