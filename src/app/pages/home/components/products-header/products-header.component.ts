12352ba3d498c04ce3b66e6593b7c26e1bd35c61
Plan49 <anthonyplanisek1@gmail.com>
2023-04-15 15:21:45 -0400

    new componet for filters

--- src/app/pages/home/components/products-header/products-header.component.ts
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: []
 })
 export class ProductsHeaderComponent {
+  @Output() columsCountChange = new EventEmitter<number>();
   sort ='sort';
+  itemsShowCount = 12
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSortUpdate(newSort: string): void {
+    this.sort = newSort
+  }
+
+  onItemsUpdate(count: number): void {
+    this.itemsShowCount = count;
+  }
+
+  onColumsUpdate(colsNum: number): void {
+    this.columsCountChange.emit(colsNum)
+  }
 }
