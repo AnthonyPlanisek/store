92ca7e1360bf1cffbdc40e646b6b8cad6f2e5dd0
Plan49 <anthonyplanisek1@gmail.com>
2023-04-16 11:42:46 -0400

    new component

---
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-box',
+  templateUrl: './product-box.component.html'
+})
+export class ProductBoxComponent {
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+}
