5e0fed6e4d38ef2cf853397537a646c40f9a6d2f
Plan49 <anthonyplanisek1@gmail.com>
2023-07-02 13:30:53 -0400

    added stripe payment

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -31,7 +33,7 @@ export class CartComponent {
     'total',
     'action'
   ]
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.cartService.cart.subscribe((_cart: Cart) => {
@@ -58,5 +60,16 @@ export class CartComponent {
 
   onRemoveQuantity(item: CartItem): void {
     this.cartService.removeQuantity(item)
+  }
+
+  onCheckout(): void {
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe(async(res:any) => {
+      let stripe = await loadStripe('pk_test_51NOMWoD0ngWjDEPQKRC4hKcaBgYhD9iJ6joni8MFfr2fEKYqfG0lc3ETTHlghPFUF7zpf1DUpiEcBQK1LD6UT7Pj008LwaieUr')
+      stripe?.redirectToCheckout({
+        sessionId: res.id
+      })
+    })
   }
 }
